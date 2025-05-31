e8a248b388c54914751725be63c755ba8671e237
fmahadyBD <fmahady01@gmail.com>
2025-06-01 00:29:53 +0600

    Initial Commit

---
+++ src/test/java/com/example/jenkins_test_2/JenkinsTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_test_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
